28f35870fdce1b4ada1208c737ecc104c58fcbf8
Belloshehu <belloshehu1@gmail.com>
2024-03-26 07:45:56 +0100

    Add student schema

---
+++ src/models/student.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const studentSchema = new mongoose.Schema({
+  faculty: {
+    type: String,
+    enum: {
+      values: ['science', 'social sciences', 'arts', 'engineering', 'agricultural science', 'law'],
+      message: '{VALUE} is not a valid faculty',
+    },
+    required: true,
+  },
+  department: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  level: {
+    type: Number,
+    default: 100,
+    required: true,
+  },
+  semester: {
+    type: Number,
+    default: 1,
+    max: [3, 'Semester too large'],
+    min: [1, 'Semester too small'],
+    required: true,
+  },
+});
+
+module.exports = mongoose.models.Student || mongoose.model('Student', studentSchema);
