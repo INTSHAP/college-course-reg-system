d953bff680dd558debbedcdf8c250cf8dd9fb1c1
Belloshehu <belloshehu1@gmail.com>
2024-04-11 14:40:46 +0100

    Add delete course end point

--- src/services/course.service.js
+++ src/services/course.service.js
@@ -43,7 +43,7 @@ const updateCourse = async (courseBody, courseId) => {
  * @param {ObjectID} courseID
  * @returns {Promise<Course>}
  */
-const deleteeCourse = async (courseId) => {
+const deleteCourse = async (courseId) => {
   // delete course with a given ID
   const existingCourse = await Course.findOne({ _id: courseId });
   if (!existingCourse) {
@@ -81,7 +81,7 @@ const getAllCourses = async (options) => {
 module.exports = {
   createCourse,
   updateCourse,
-  deleteeCourse,
+  deleteCourse,
   getSingleCourse,
   getAllCourses,
 };
